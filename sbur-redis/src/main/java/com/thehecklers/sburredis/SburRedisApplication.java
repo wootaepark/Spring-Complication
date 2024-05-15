ea9d29793f090182504804820b77aa445f08e904
wootaepark <pakdal99@gmail.com>
2024-05-15 21:24:51 +0900

    Ch6_1-commit

---
+++ sbur-redis/src/main/java/com/thehecklers/sburredis/SburRedisApplication.java
@@ -0,0 +1,13 @@
+package com.thehecklers.sburredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SburRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SburRedisApplication.class, args);
+	}
+
+}
